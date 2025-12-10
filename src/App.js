4e50ac8a083c2de31fe106b4659ac3aa25ba5674
Руслан Хинчагов <ruslanhincagov@MacBook-Pro-Ruslan.local>
2025-12-11 02:44:41 +0300

    interim commit

--- src/App.js
+++ src/App.js
@@ -5,61 +5,27 @@ import './App.css';
 import ProgressHeader from './components/ProgressHeader';
 import QuickActions from './components/QuickActions';
 import TechnologyNotes from './components/TechnologyNotes';
+import useTechnologies from './hooks/useTechnologies';
 
 
 function App() {
-  const [cards, setCards] = useState([
-    {id:1, title: "Сделать практику 1-19", description: "Сделать технологии карточек и добавить им стили", status: "completed", notes: 'Нужно чет сделать до завтра'},
-    {id:2, title: "Научиться использовать React свободно", description: "Разобраться в разновидности хуков и понять как все работает под капотом", status: "in-progress", notes: 'Что-то крутое'},
-    {id:3, title: "Изучение Vue", description: "Начат изучение Vue", status: "not-started", notes: 'полезная тема'},
-    {id:4, title: "Изучение Ts", description: "Начать изучение TypeScript", status: "not-started", notes: ''}
-  ])
+
+  const {technologies, updateStatus, updateNotes, progress} = useTechnologies();
 
 
   const [searchQuery, setSearchQuery] = useState('');
 
-
-
-  useEffect(() => {
-    const saved = localStorage.getItem('techTrackerData');
-    if(saved){
-      setCards(JSON.parse(saved)); 
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('techTrackerData', JSON.stringify(cards));
-  }, [cards]);
-
-  const UpdateTechnologyNotes = (cardId, newNotes) => {
-    setCards(prevCards => 
-      prevCards.map(card => 
-        card.id === cardId ? {...card, notes: newNotes} : card
-      )
-    )
-  }
-
   const [filter, setFilter] = useState('all')
 
-  const filteredCards = cards.filter(card => 
-    filter === 'all' ? true : card.status === filter)
-    .filter(card => card.title.toLowerCase().includes(searchQuery.toLowerCase())
-    || card.description.toLowerCase().includes(searchQuery.toLowerCase()))
+  const filteredCards = technologies.filter(tech => 
+    filter === 'all' ? true : tech.status === filter)
+    .filter(tech => tech.title.toLowerCase().includes(searchQuery.toLowerCase())
+    || tech.description.toLowerCase().includes(searchQuery.toLowerCase()))
   
-
-  const updateCardStatus = (cardId, newStatus) => {
-    setCards(prevCards => 
-      prevCards.map(card => 
-        card.id === cardId
-          ? { ...card, status: newStatus } 
-          : card
-      )
-    )
-  }
 
   return (
     <div className="App">
-      <ProgressHeader cards={cards}/>
+      <ProgressHeader cards={technologies}/>
       <div className='search-box'>
         <input
           type='text'
@@ -71,7 +37,7 @@ function App() {
         <span className='search-box__count'>Найдено: {filteredCards.length}</span>
       </div>
 
-      <QuickActions cards={cards} onChangeCards={setCards} />
+      <QuickActions cards={technologies} onChangeCards={updateStatus} />
 
       <div className='filters'>
         <button className={filter === 'all' ? 'active' : ''} onClick={() => setFilter('all')}>
@@ -88,11 +54,11 @@ function App() {
         </button>
       </div>
       <div className="technology-cards__grid">
-        {filteredCards.map(card => 
-        <React.Fragment key={card.id}>
-          <TechnologyCard cards={card} key={card.id} 
-          onStatusChange={(newStatus) => updateCardStatus(card.id, newStatus)}/>
-          <TechnologyNotes notes={card.notes} onNotesChange={UpdateTechnologyNotes} techId={card.id}/>
+        {filteredCards.map(tech => 
+        <React.Fragment key={tech.id}>
+          <TechnologyCard cards={tech} key={tech.id} 
+          onStatusChange={(newStatus) => updateStatus(tech.id, newStatus)}/>
+          <TechnologyNotes notes={tech.notes} onNotesChange={updateNotes} techId={tech.id}/>
         </React.Fragment>
         )}
       </div>
